35379e6f05cf4db0203ea91c6557eda42556dedd
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2026-01-21 16:37:37 +0600

    Update gallery images to use distinct assets

--- js/script.js
+++ js/script.js
@@ -256,7 +256,7 @@ const galleryData = [
         description: 'The Metro Rail, a symbol of modern development, resumes operations as the city returns to normalcy under new leadership.'
     },
     {
-        image: 'images/protest.png',
+        image: 'images/ph-protest.svg',
         title: 'Shahbag Square Protests',
         description: 'Historic gathering at Shahbag intersection, a symbolic location for Bangladesh\'s democratic movements.'
     },
@@ -266,7 +266,7 @@ const galleryData = [
         description: 'Formation of the new administration with advisers from various sectors focused on reforms and stability - August 8, 2024.'
     },
     {
-        image: 'images/celebration.png',
+        image: 'images/ph-celebration.svg',
         title: 'National Pride',
         description: 'Citizens wave Bangladesh flag celebrating the peaceful democratic transition and hope for the future.'
     }
